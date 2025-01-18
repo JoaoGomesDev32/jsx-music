d49a7e08a8d634b67e177a0b861c1f8dadce750e
João Felipe <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-01-18 15:29:29 +0000

    :rocket:
    create initials pages

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,6 @@
+const HomePage = () => {
+    return <div>Welcome to JSX Music</div>;
+  };
+  
+  export default HomePage;
+  
---
+++ src/pages/MusicPlayerPage.tsx
@@ -0,0 +1,6 @@
+const MusicPlayerPage = () => {
+    return <div>Music Player</div>;
+  };
+  
+  export default MusicPlayerPage;
+  
