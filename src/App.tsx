5e6f7a27c9d732841577b5c9fae8592967baee9f
João Felipe <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-01-18 15:36:52 +0000

    :lipstick:
    theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import MusicPlayerPage from './pages/MusicPlayerPage';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#1db954', // Cor verde do Deezer/Spotify
+    },
+    background: {
+      default: '#121212',
+      paper: '#1c1c1c',
+    },
+  },
+});
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/player/:id" element={<MusicPlayerPage />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/player/:id" element={<MusicPlayerPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
